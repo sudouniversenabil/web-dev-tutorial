3d65408df172398ebb44e62c79a0fdf592035e6f
sudouniverse <nabilsikder00@gmail.com>
2026-04-21 16:47:42 +0600

    21day

---
+++ javascript/Classes & Objects - Object Oriented Programming in Js/js.js
@@ -0,0 +1,19 @@
+let obj ={
+a:1,
+b:"nabil"
+}
+
+console.log(obj)
+
+let animal ={
+    eats :true
+}
+
+let rabbit={
+jumps:true
+}
+
+rabbit.__proto__=animal // that is a proto type funtion 
+
+
+console.log(rabbit)
--- javascript/JavaScript try catch & Error Handling /js.js
+++ javascript/JavaScript try catch & Error Handling /js.js
@@ -1,9 +1,28 @@
-let a = prompt("input the sec number")
-let b =prompt("input the sec number")
+let a = prompt("input your number")
 
-let sum = parseInt(a)+parseInt(b)
-if (isNaN(a) || isNaN(b)){
-    throw SyntaxError("this is a wong input plese input the number type")
-}else{
-console.log("sum is ",sum)
+let b = prompt("input your sec number")
+// if (isNaN(a) || isNaN(b)){
+//     throw SyntaxError("sorry this is a sysntex eroer")
+// }
+
+let sum = parseInt(a) + parseInt(b)
+
+console.log(sum)
+
+let era = () => {
+    let x = 4
+    try {
+        console.log("new number add", sum * x)
+        return true
+
+    } catch (error) {
+        console.log("sorry x is not defined")
+        return false
+    }
+
+    finally {
+        console.log("iam finally funtion code is run ")
+    }
 }
+
+let ff = era()
