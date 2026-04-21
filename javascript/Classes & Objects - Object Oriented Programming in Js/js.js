4a4507c9998088a011abf4e95b475dc632e2768f
sudouniverse <nabilsikder00@gmail.com>
2026-04-21 18:05:44 +0600

    last

--- javascript/Classes & Objects - Object Oriented Programming in Js/js.js
+++ javascript/Classes & Objects - Object Oriented Programming in Js/js.js
@@ -1,19 +1,55 @@
-let obj ={
-a:1,
-b:"nabil"
+// let obj ={
+// a:1,
+// b:"nabil"
+// }
+
+// console.log(obj)
+
+// let animal ={
+//     eats :true
+// }
+
+// let rabbit={
+// jumps:true
+// }
+
+// rabbit.__proto__=animal // that is a proto type funtion 
+
+
+// console.log(rabbit)
+
+class animal{
+    constructor(name){
+        this.nam=name
+        // this.jara=jara
+        console.log("this object is create done..." ," and my name is lion "+name)
+    }
+
+    eats(){
+        console.log("ami js laer n korte")
+    }
+    pytohn(){
+        console,log("my favrete language is python ")
+    }
 }
 
-console.log(obj)
+class king_lion extends animal{
+        constructor(name,jara){
+            super(name)
+        this.nam=name
+        this.jara=jara
+        console.log("this object is create done..." ," and my name is rabbit "+name +jara)
+    }
 
-let animal ={
-    eats :true
 }
 
-let rabbit={
-jumps:true
-}
+let a = new animal("bunny")
 
-rabbit.__proto__=animal // that is a proto type funtion 
+let b =new king_lion("sera","nabil")
+console.log(a)
+console.log(b)
+
+// console.log(the)
 
 
-console.log(rabbit)
+
