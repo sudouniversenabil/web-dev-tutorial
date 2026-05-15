8b9b8b162408febd6fd4d1a18fcc8ad12cb91281
sudouniverse <nabilsikder00@gmail.com>
2026-05-15 17:04:31 +0600

    update

--- backend/Response, Request and Routers in Express  4/main.js
+++ backend/Response, Request and Routers in Express  4/main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// const local=173.1
 app.use(express.static('public')) // for static files like css, js, images etc. in public folder
 
 app.get('/', (req, res) => {
@@ -8,10 +9,26 @@ app.get('/', (req, res) => {
 })
 app.post('/', (req, res) => {
   console.log("hey this is a post request")
-  // req.send('hallo world it is a get request')
+  res.send('hallo world it is a post request')
+})
+app.put('/', (req, res) => {
+  console.log("hey this is a put request")
+  res.send('hallo world it is a put request')
 })
 
+// app.delete('/', (req, res) => {
+//   console.log("hey this is a delete request")
+//   res.send('hallo world it is a delete request')
+// })
+
+app.get("/bolog", (req, res) => {
+  res.sendFile("templete/bolog.html",{root:__dirname})
+  console.log("done")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 // console.log("haloo")
+
+
+// https://youtu.be/P_17PQFpIAY?si=aHM7ifIEA_I9IS9m
