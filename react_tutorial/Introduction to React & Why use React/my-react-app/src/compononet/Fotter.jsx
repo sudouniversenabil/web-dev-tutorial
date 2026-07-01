0546270eadd7954cdda69ce007bc198f625a3792
sudouniverse <nabilsikder00@gmail.com>
2026-07-01 22:20:02 +0600

    update

---
+++ react_tutorial/Introduction to React & Why use React/my-react-app/src/compononet/Fotter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const fotter = () => {
+  return (
+    <div>
+      <ul>
+        <li>end 1</li>
+        <li>end 2</li>
+        <li>py3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default fotter
---
+++ react_tutorial/Introduction to React & Why use React/my-react-app/src/compononet/Navber.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Fotter from './Fotter.jsx'
+const navber = () => {
+  return (
+    <div>
+      <ul>
+        <li>nabil</li>
+        <li>css</li>
+        <li>python</li>
+      </ul>
+      <Fotter />
+    </div>
+  )
+}
+
+export default navber
