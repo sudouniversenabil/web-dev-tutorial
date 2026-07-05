59d4488c07eea2b3de42fb833acf7b495f2e0325
sudouniverse <nabilsikder00@gmail.com>
2026-07-05 18:04:28 +0600

    update

---
+++ react_tutorial/component_ Props_and_JSX_in_React/src/App.jsx
@@ -0,0 +1,12 @@
+import Navber from "./components/Navber"
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navber/>
+    </>
+  )
+}
+
+export default App
---
+++ react_tutorial/component_ Props_and_JSX_in_React/src/components/Navber.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Navber.css"
+function Navber() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>contatat</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navber
