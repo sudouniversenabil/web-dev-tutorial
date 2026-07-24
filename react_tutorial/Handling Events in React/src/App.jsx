779b545f1819a7b3d0ae46c6a12525165a42432a
sudouniversenabil <nabilsikder00@gmail.com>
2026-07-24 16:41:31 +0600

    react

---
+++ react_tutorial/Handling Events in React/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [name, setname] = useState("harry")
+  const [form, setform] = useState({ email: "", phone: "" })
+
+  const handEvent = () => {
+    alert("hey i am click")
+  }
+
+  const over = () => {
+    alert("iam e mouse over")
+  }
+
+  // name স্টেটের জন্য হ্যান্ডলার
+  const handleNameChange = (e) => {
+    setname(e.target.value)
+  }
+
+  // form অবজেক্টের (email, phone) জন্য হ্যান্ডলার
+  const new_add = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value })
+    console.log(form)
+  }
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handEvent}>click me </button>
+      </div>
+
+      <div className='over' onMouseOver={over}>iam another event</div>
+
+      {/* Name Input */}
+      <input 
+        type="text" 
+        value={name} 
+        onChange={handleNameChange} 
+      />
+
+      {/* Email Input */}
+      <input 
+        type="text" 
+        name='email' 
+        value={form.email || ""} 
+        onChange={new_add} 
+      />
+
+      {/* Phone Input */}
+      <input 
+        type="text" 
+        name='phone' 
+        value={form.phone || ""} 
+        onChange={new_add} 
+      />
+    </>
+  )
+}
+
+export default App
